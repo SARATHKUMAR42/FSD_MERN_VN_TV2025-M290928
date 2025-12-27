961a2ba2128d356b74172ace2a24360ec5d8c32e
SARATH KUMAR <169058871+SARATHKUMAR42@users.noreply.github.com>
2025-12-27 11:56:30 +0530

    Add files via upload

---
+++ Project2/MediBook/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/layout.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 Medibook. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
